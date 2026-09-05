d1669a357b1f6074e283cc633089a96e55055464
DEVI JEYA <devijeyavisalaxi@gmail.com>
2026-09-05 23:49:00 +0530

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        
+        int res=0,num=x;
+        while(num!=0){
+            
+            
+            res=(res*10)+(num%10);
+            num/=10;
+            
+
+        }
+        return res==x;
+        
+    }
+}
